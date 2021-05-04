62f440e89f3c6e71a792a0bc26de94e63a6244ba
pablohernandezrom <pablohernandezrom@gmail.com>
2021-05-04 15:06:38 -0300

    Agregué formularios de registro y login

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath)); 
 
-app.listen(3050, () => {
-    console.log("El Servidor esta corriendo en el puerto 3050")
+app.listen(1050, () => {
+    console.log("El Servidor esta corriendo en el puerto 1050")
 })
 
 app.get("/", (req,res) =>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/registro", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/login", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
 })
